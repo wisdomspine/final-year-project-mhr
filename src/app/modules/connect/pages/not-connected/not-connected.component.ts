6186de208f6c09c7cf1a73083797eb4564e9738d
Priest Ombugadu <sabopriest@gmail.com>
2023-04-24 21:22:03 +0100

    feat: adds algorand and walletconnect dependencies, adds global polyfill

--- src/app/modules/connect/pages/not-connected/not-connected.component.ts
+++ src/app/modules/connect/pages/not-connected/not-connected.component.ts
@@ -1,5 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
+import WalletConnect from '@walletconnect/client';
+import QRCodeModal from 'algorand-walletconnect-qrcode-modal';
 
 @Component({
   selector: 'app-not-connected',
