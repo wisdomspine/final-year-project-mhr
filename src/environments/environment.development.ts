65991f49bff607aaf0062801ec5abfa0e790c179
Priest Ombugadu <sabopriest@gmail.com>
2023-04-26 09:33:33 +0100

    feat: generate env files, and infura env configuration

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  infura: {
+    key: '2OxMVPnDOC2D7Uh5MalT5pU6rBZ',
+    secret: '4ef27ea58361c7e29afd80f24720e4ee',
+    endpoint: 'https://ipfs.infura.io:5001',
+  },
+};
